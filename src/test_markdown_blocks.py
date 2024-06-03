39c6626c67a1c3f9407118a0110a62a49663771e
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-06-03 16:27:57 +0200

    write block to block type and tests

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,8 +1,31 @@
 import unittest
 from markdown_blocks import (
   markdown_to_blocks,
+  block_to_block_type,
+  block_type_paragraph,
+  block_type_heading,
+  block_type_code,
+  block_type_ordered_list,
+  block_type_unordered_list,
+  block_type_quote,
 )
 
-def test_markdown_to_blocks(self):
-  blocks = markdown_to_blocks("This is a text node\n\nThis is another text node")
-  self.assertListEqual(["This is a text node", "This is another text node"], blocks)
+
+class TestMarkdownToHTML(unittest.TestCase):
+  def test_markdown_to_blocks(self):
+    blocks = markdown_to_blocks("This is a text node\n\nThis is another text node")
+    self.assertListEqual(["This is a text node", "This is another text node"], blocks)
+
+  def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), block_type_code)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), block_type_quote)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), block_type_unordered_list)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), block_type_ordered_list)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
