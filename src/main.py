0b1bdbf806e38479c98f57b65f289f63e6fc9eea
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-06-03 16:11:44 +0200

    write split nodes link and image, and tests

--- src/main.py
+++ src/main.py
@@ -6,12 +6,20 @@ from textnode import (
     text_type_code,
 )
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from inline_markdown import split_nodes_delimiter, extract_markdown_images,extract_markdown_links
+from inline_markdown import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 # The main function of the script
 def main():
   text = TextNode("This is a text node", "bold", "https://www.google.com")
   print(text)
+
+  node = TextNode(
+    "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+    text_type_text,
+  )
+  new_nodes = split_nodes_link([node])
+  print(f"Split nodes: {new_nodes}")
+
 
 
 main()
